be3b64489276a384eb6670b8289b56f604596fa6
GayshaxD <62309533+GayshaxD@users.noreply.github.com>
2021-11-10 23:32:19 -0600

    4.10.11.2021
    Se agregaron Datos estáticos a las tablas reservas y habitaciones. Así mismo se agregó una tabla al componente reservas.

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -3,15 +3,45 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { async } from '@firebase/util';
 import { AuthService } from '../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { ViewEncapsulation } from '@angular/core';
+
+export interface Data {
+  reservas: string;
+}
+
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
   styleUrls: ['./admin.page.scss'],
-})
+  encapsulation: ViewEncapsulation.None
+}) 
 export class AdminPage{
+  public data: Data;
+  public columns: any;
+  public rows: any;
 
-  constructor(private router:Router, private authSvc:AuthService) { }
+  constructor(private router:Router, private authSvc:AuthService, private http: HttpClient) { 
+    this.columns = [
+      { name: 'ID' },
+      { name: 'FECHA' },
+      { name: 'CLIENTE' },
+      { name: 'HABITACIONES' },
+      { name: 'EMPLEADO' },
+      { name: 'TOTAL' },
+      { name: 'VER MAS', prop: 'Id'}
+    ];
+
+    this.http.get<Data>('../../assets/reservas.json')
+      .subscribe((res) => {
+        console.log(res)
+        
+        this.rows = res.reservas;
+       
+      });
+
+  }
 
   async onLogout(){
     try {
